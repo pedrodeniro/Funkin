6b9c23ebf74e49a51c277d1378f23162fffa747f
Pedro De Niro <neil@treherbert.com>
2021-07-19 17:49:53 +0100

    Intro changes

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -364,52 +364,52 @@ class TitleState extends MusicBeatState
 
 		switch (curBeat)
 		{
-			case 1:
-				createCoolText(['Furby', 'Furby', 'Jocelyn', 'Furby']);
+			case 2:
+				createCoolText(['Made by', 'a', 'Nine Yr Old']);
 			// credTextShit.visible = true;
-			case 3:
-				addMoreText('present');
+			case 4:
+				addMoreText('Please be Kind!');
 			// credTextShit.text += '\npresent...';
 			// credTextShit.addText();
-			case 4:
+			case 5:
 				deleteCoolText();
 			// credTextShit.visible = false;
 			// credTextShit.text = 'In association \nwith';
 			// credTextShit.screenCenter();
-			case 5:
+			case 6:
 				createCoolText(['In association', 'with']);
-			case 7:
+			case 8:
 				addMoreText('Grumpy Dad');
 				ngSpr.visible = true;
 			// credTextShit.text += '\nNewgrounds';
-			case 8:
+			case 9:
 				deleteCoolText();
 				ngSpr.visible = false;
 			// credTextShit.visible = false;
 
 			// credTextShit.text = 'Shoutouts Tom Fulp';
 			// credTextShit.screenCenter();
-			case 9:
+			case 10:
 				createCoolText([curWacky[0]]);
 			// credTextShit.visible = true;
-			case 11:
+			case 12:
 				addMoreText(curWacky[1]);
 			// credTextShit.text += '\nlmao';
-			case 12:
+			case 13:
 				deleteCoolText();
 			// credTextShit.visible = false;
 			// credTextShit.text = "Friday";
 			// credTextShit.screenCenter();
-			case 13:
+			case 14:
 				addMoreText('Furby');
 			// credTextShit.visible = true;
-			case 14:
+			case 15:
 				addMoreText('Night');
 			// credTextShit.text += '\nNight';
-			case 15:
+			case 16:
 				addMoreText('Funkin'); // credTextShit.text += '\nFunkin';
 
-			case 16:
+			case 17:
 				skipIntro();
 		}
 	}
